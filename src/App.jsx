7db0385f2147852eb967e3c0b4fd074b81cefc4a
Aad1tya27 <agarwal.aaditya2765@gmail.com>
2024-06-30 23:11:38 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+function App() {
+  
+  return (
+    
+    <>
+      <main className='bg-black flex flex-col h-screen w-screen text-white justify-center items-center text-[40px]'>
+        <div>Hi, I am Aaditya Agarwal and I am a</div>
+        <ul className='text-center text-[30px]'>
+          <li>Web Developer</li>
+          <li>Guitarist</li>
+          <li>Tennis Player</li>
+        </ul>
+      </main>
+      
+    </>
+  )
+}
+
+export default App
