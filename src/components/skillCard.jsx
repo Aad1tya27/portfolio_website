bc54027efe8e8ad19e808c6030e825ab3e03b5bf
Aad1tya27 <agarwal.aaditya2765@gmail.com>
2024-12-17 03:21:23 +0530

    added hover effect

--- src/components/skillCard.jsx
+++ src/components/skillCard.jsx
@@ -5,7 +5,7 @@ const SkillCard = ({ name, children }) => {
         {
             name!="" ?
                 <>
-                    <div className=" text-center w-[200px] lg:w-auto  bg-gray-800  bg-opacity-30 text-xl  p-3 px-6 text-white">
+                    <div className=" text-center w-[200px] lg:w-auto  bg-gray-800 hover:bg-gray-900  bg-opacity-30 text-xl  p-3 px-6 text-white">
                         < div className='flex justify-center items-center' >
                             <h2 className='text-kg font-medium p-2'>{name}</h2>
                             {children}
@@ -16,7 +16,7 @@ const SkillCard = ({ name, children }) => {
                 </>
                 :
                 <>
-                <div className=" w-[200px] lg:w-auto  bg-gray-800  bg-opacity-30 flex justify-center items-center p-3 px-6   ">
+                <div className=" w-[200px] lg:w-auto  bg-gray-800  bg-opacity-30 hover:bg-gray-900 flex justify-center items-center p-3 px-6   ">
                     {children}
                 </div >
                 </>
